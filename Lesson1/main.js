be84c1a0598bb3150e52dc1c99850a4c24314cd7
HelenaYaniuk <olena.voytovych41@gmail.com>
2022-02-01 15:16:40 +0200

    Initial commit

---
+++ Lesson1/main.js
@@ -0,0 +1,87 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+let hello ='hello';
+let owu='owu';
+let com='com';
+let d='ua';
+let e='1';
+let f='10';
+let g='-999';
+let h='123';
+let i='3.14';
+let j='2.7';
+let k='16';
+let l='true'
+let m='false';
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+hello='Hello';
+owu='oWu';
+com='coM';
+d='UA';
+e='5';
+f='180';
+g='-99';
+h='123456';
+i='3.9';
+j='7.7';
+k='56';
+l='trUE'
+m='faLse';
+console.log (hello);
+console.log(owu);
+console.log(com);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+let firstName='Янюк';
+let lastName='Олена';
+let middleName='Русланівна';
+let person=['firstName','lastName','middleName'];
+console.log(person)
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+console.log(5<6);
+console.log(5>6);
+console.log(5===6);
+console.log(5>=6);
+console.log(10===10);
+console.log(10>=10);
+console.log(10!==10);
+console.log(10>10);
+console.log(10<10);
+console.log(123 ==='123');
+console.log(123 !== '123');
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// - не розумію докінця, що тут робити
